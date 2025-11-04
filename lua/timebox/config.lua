999036be804f90756c950d2cad70fa391f8db906
Linus <contact@linusskold.com>
2025-11-04 02:05:16 +0100

    feat(config): Added customizeable messages to config.lua

--- lua/timebox/config.lua
+++ lua/timebox/config.lua
@@ -5,7 +5,14 @@ M.defaults = {
 		work = 25 * 60 * 1000,
 		coffee = 5 * 60 * 1000,
 	},
-	notifications = true, -- enable/disable notifications
+	notifications = true, -- enable/disable notificatios
+
+	messages = {
+		start_work = "Time to focus! Work session started.",
+		end_work = "Great job! Work session ended.",
+		start_coffee = "Take a break! Coffee session started.",
+		end_coffee = "Break over! Coffee session ended.",
+	},
 }
 
 M.options = {}
