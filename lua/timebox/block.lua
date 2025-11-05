cc0af5ffc06892bb5fd048216bf9e2f54495fb1f
linus.skold <contact@linusskold.com>
2025-11-05 17:32:13 +0100

    feat(block): Implemented a block class with clear functions and usage

---
+++ lua/timebox/block.lua
@@ -0,0 +1,40 @@
+---@class Block
+---@field name string
+---@field start_time number
+---@field end_time number
+---@field elapsed number
+---@field block_type string
+
+local timer = require("timebox.timer")
+
+local M = {}
+M.__index = M
+
+function M.new(name, block_type, timer)
+	local self = setmetatable({}, M)
+	self.name = name
+	self.block_type = block_type
+	self.timer = timer
+	return self
+end
+
+function M:start()
+	self.start_time = os.time()
+	self.timer:start()
+end
+
+function M:stop()
+	self.end_time = os.time()
+	self.elapsed = self.end_time - self.start_time
+	self.timer:stop()
+end
+
+function M:pause()
+	self.timer:pause()
+end
+
+function M:resume()
+	self.timer:resume()
+end
+
+return M
